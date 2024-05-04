8d494ca35e1c356d9934d053830940e9c650ac63
Jesús Leganés-Combarro 'piranna <piranna@gmail.com>
2024-05-04 11:57:35 +0200

    Fix indentation

--- scripts/update.js
+++ scripts/update.js
@@ -43,26 +43,26 @@ export function serializeProtocol(protocol: TransportProtocol): FbsTransport.Pro
   }
 }`
 const utilsTs = `/**
-* Make an object or array recursively immutable.
-* https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/freeze.
-*/
+ * Make an object or array recursively immutable.
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/freeze.
+ */
 export function deepFreeze<T>(object: T): T
 {
- // Retrieve the property names defined on object.
- const propNames = Reflect.ownKeys(object as any);
+  // Retrieve the property names defined on object.
+  const propNames = Reflect.ownKeys(object as any);
 
- // Freeze properties before freezing self.
- for (const name of propNames)
- {
-   const value = (object as any)[name];
+  // Freeze properties before freezing self.
+  for (const name of propNames)
+  {
+    const value = (object as any)[name];
 
-   if ((value && typeof value === 'object') || typeof value === 'function')
-   {
-     deepFreeze(value);
-   }
- }
+    if ((value && typeof value === 'object') || typeof value === 'function')
+    {
+      deepFreeze(value);
+    }
+  }
 
- return Object.freeze(object);
+  return Object.freeze(object);
 }`
 
 
