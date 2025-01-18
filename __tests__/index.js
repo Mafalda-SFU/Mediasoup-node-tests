4911997e7117dbe8cb364a7f7689f2e4c60d059f
Jesús Leganés-Combarro 'piranna <piranna@gmail.com>
2025-01-18 21:57:31 +0100

    Fix tests with prereleases

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,5 +1,14 @@
+const {readFileSync} = require('fs');
+const {join} = require('path');
+
 const mediasoupNodeTests = require('@mafalda-sfu/mediasoup-node-tests');
 const mediasoup = require('mediasoup');
+const {sync} = require('pkg-dir');
+
+
+const {version} = JSON.parse(
+  readFileSync(join(sync(__dirname), 'package.json'), {encoding: 'utf-8'})
+);
 
 
 test('layout', function()
@@ -7,4 +16,4 @@ test('layout', function()
   expect(mediasoupNodeTests).toMatchInlineSnapshot(`[Function]`)
 })
 
-mediasoupNodeTests(mediasoup)
+mediasoupNodeTests({...mediasoup, version})
